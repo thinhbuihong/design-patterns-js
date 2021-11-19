59e0a674f13e386d6b9c3ac5369c3efc921b4db7
John Doe <thinhbuihong.ma@gmail.com>
2021-11-19 20:08:08 +0700

    first commit

---
+++ singleton.js
@@ -0,0 +1,23 @@
+const Singleton = (function () {
+	let pManager
+
+	function ProcessManager() { /*...*/ }
+
+	function createProcessManager() {
+		pManager = new ProcessManager()
+		return pManager
+	}
+
+	return {
+		getProcessManager: () => {
+			if (!pManager)
+				pManager = createProcessManager()
+			return pManager
+		}
+	}
+})()
+
+const singleton = Singleton.getProcessManager()
+const singleton2 = Singleton.getProcessManager()
+
+console.log(singleton === singleton2)
